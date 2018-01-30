91a1772e11fe882c432c3ab0df448f3601b9ec32
shooterdz <mehidinechupeau@gmail.com>
2018-01-30 14:26:26 +0100

    ajout strat sequentiel

--- src/stategy/SequentialStrat.java
+++ src/stategy/SequentialStrat.java
@@ -1,11 +1,18 @@
 package stategy;
 
+import java.util.List;
+
+import observer.Capteur;
+import observer.ObserverAsync;
+
 public class SequentialStrat extends AlgoDiffusion{
 
 	@Override
 	public void execute() {
-		// TODO Auto-generated method stub
-		
+		List<ObserverAsync<Capteur>> list = this.capteur.getListObserver();
+		for(ObserverAsync<Capteur> c : list){
+			c.update(capteur);
+		}
 	}
 
 }
